6995ba6939f04c0203906d99ba1ca5950b75b4fa
BeautifulCodes <pankajmishrajec@gmail.com>
2018-06-02 16:36:29 +0530

    Accepted 6ms

---
+++ find the difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int xr=0;
+        s+=t;
+        for(int i=0;i<s.length();i++){
+            xr^=(s[i]-'a');
+        }
+        return (char(xr+'a'));
+    }
+};
