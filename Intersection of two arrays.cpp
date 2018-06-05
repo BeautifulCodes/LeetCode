12166f26582da70aee71157d81180666d2270247
BeautifulCodes <pankajmishrajec@gmail.com>
2018-06-05 15:43:49 +0530

    Cleaner solution, O(Mlog(N)) with auxilary space O(Intersection(M,N)), Improved 20%

--- Intersection of two arrays.cpp
+++ Intersection of two arrays.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int>included;
-        int i=0,j=0;
         vector<int>ans;
+        map<int,int>included;
+        if(nums1.empty() or nums2.empty())return ans;
         sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        while(i<nums1.size() and j<nums2.size()){
-            if(nums1[i]==nums2[j]){
-                if(!included[nums1[i]]){
-                    ans.push_back(nums1[i]);
-                    included[nums1[i]]=true;
-                }
-                i++,j++;
-            }else if(nums1[i] < nums2[j]){
-                i++;
-            }else{
-                j++;
+        for(int i=0;i<nums2.size();i++){
+            if(!included[nums2[i]] && binary_search(nums1.begin(),nums1.end(),nums2[i])){
+                ans.push_back(nums2[i]);
+                included[nums2[i]]=true;
             }
         }
         return ans;
